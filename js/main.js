5d6cf5c242da9686513a62221c7d7df62596c430
MuradHamidov302 <hmurad.p302@code.edu.az>
2017-08-21 01:52:24 +0400

    add page blog-1 and add page single-article-1 -finish

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
   //id reating------------------
 
 
-   // $(".listing-item-link").hover(function(){
-   //     $( ".listing-item-rating").animate({top: '0px'}).fadeOut();
-   //     $( ".category-icon ").animate({top: '-20px'}).fadeOut();
-	  //    $(".listing-item-date").animate({top: '-20px'}).fadeOut();
-   //      });
+   $(".listing-item-link").hover(function(){
+   $(this).parent().children(".listing-item-date").animate({top: '-20px',opacity:'0'},50);
+   $(this).parent().children(".category-icon ").animate({top: '-20px',opacity:'0'},50);
+   $(this).parent().children(".listing-item-rating ").animate({top: '20px',opacity:'0'},50);
 
-   //      $(".listing-item-link").mouseout( function(){
-   //    $(".listing-item-rating").fadeIn().animate({top: '12%'});
-   //     $(".category-icon ").fadeIn().animate({top: '0px'});
-   //     $( ".listing-item-date").fadeIn().animate({top: '0px'});
-	  
-   //  });
-
+ }, function () {
+         
+      $(this).parent().children(".listing-item-date").animate({top: '0px',opacity:'1'},50);
+       $(this).parent().children("#reyting .category-icon ").animate({top: '0px',opacity:'1'},50);
+       $(this).parent().children("#yeniler .category-icon ").animate({top: '11px',opacity:'1'},50);
+        $(this).parent().children(".listing-item-rating ").animate({top: '12%',opacity:'1'},50);
+        });
+     
 //id reating end---------------
 
 //scrollhome page start-----------------
