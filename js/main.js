dabd4a98bdf172971b26862ff77277102b3060cd
MuradHamidov302 <hmurad.p302@code.edu.az>
2017-08-28 18:31:15 +0400

    new page add list grid 5,list grid 1, homepage slider update and add like button

--- js/main.js
+++ js/main.js
@@ -10,11 +10,11 @@
  }, function () {
          
       $(this).parent().children(".listing-item-date").animate({top: '0px',opacity:'1'},50);
-       $(this).parent().children("#reyting .category-icon ").animate({top: '0px',opacity:'1'},50);
-        $(this).parent().children("#left-content .category-icon ").animate({top: '25px',opacity:'1'},50);
-       $(this).parent().children("#yeniler .category-icon ").animate({top: '11px',opacity:'1'},50);
-        $(this).parent().children(".listing-item-rating ").animate({top: '12%',opacity:'1'},50);
-         $(this).parent().children(".listing-item-author  ").animate({top: '84px',opacity:'1'},50);
+      $(this).parent().children("#reyting .category-icon ").animate({top: '0px',opacity:'1'},50);
+      $(this).parent().children("#left-content .category-icon ").animate({top: '25px',opacity:'1'},50);
+      $(this).parent().children("#yeniler .category-icon ").animate({top: '11px',opacity:'1'},50);
+      $(this).parent().children(".listing-item-rating ").animate({top: '12%',opacity:'1'},50);
+      $(this).parent().children(".listing-item-author  ").animate({top: '84px',opacity:'1'},50);
         });
      
 //id reating end---------------
@@ -36,3 +36,52 @@ $("html,body").animate({
    });
 //scroll home page end-------------------------
 
+
+//start avtopaly slider home page section yeniler and reating
+$(document).ready(function(){
+$(".owl-carousel").owlCarousel({
+  items:4,
+
+    loop:true,
+    margin:10,
+     autoplay:true,
+    autoplayTimeout:1000,
+    autoplayHoverPause:true,
+     responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:4
+        }}
+});
+
+});
+//end avtoplay slider ---
+
+//start like button----------------------------------------------------------------------
+var like=true;
+$("#yeniler i").click(function(){
+
+if (like==true) {
+  $( this ).css( "color", "red" );
+  like=false;
+}else{
+   $( this ).css( "color", "#2AA589");
+   like=true;
+}
+
+});
+//end like button
+
+
+ //MURAD BU COMMENTE ALDIGIM YERI JAVASCRIPT FAYLINI TAPIB YERLESDIRERSEN 
+//JAVASCRIPT FAYLLARINI NE ETMISEN? HEC BIRI YOXDU 
+
+  function myFunction(){
+    document.getElementById('mySelector').css(display, 'block');
+  }
+////end
